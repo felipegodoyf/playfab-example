00f8b6fcd38f3ac8359e973c723c96bace3a150d
felipegodoyf <felipegodoyf@outlook.com>
2021-09-12 19:53:03 -0300

    Realizando login do PlayFab

---
+++ Assets/Scripts/PlayfabManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+
+public class PlayfabManager : MonoBehaviour
+{
+    public static PlayfabManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void Start()
+    {
+        Login();
+    }
+
+    public void Login()
+    {
+        Debug.Log("PlayFab: Login...");
+        PlayFabClientAPI.LoginWithCustomID
+        (
+            new LoginWithCustomIDRequest()
+            {
+                CreateAccount = true,
+                CustomId = SystemInfo.deviceUniqueIdentifier
+            },
+            response =>
+            {
+                Debug.Log("PlayFab: Logged in.");
+            },
+            error =>
+            {
+                Debug.LogError(error.GenerateErrorReport());
+                Login();
+            }
+        );
+    }
+}
